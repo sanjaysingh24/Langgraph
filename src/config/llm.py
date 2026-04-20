72d8c6ff183890d6b04f289cb0993f205a33c1a0
sanjaysingh24 <sanjubora84@gmail.com>
2026-04-20 22:53:14 +0530

    update

--- src/config/llm.py
+++ src/config/llm.py
@@ -5,10 +5,11 @@ load_dotenv()
 
 
 llm = HuggingFaceEndpoint(
-    repo_id=os.getenv("HF_LLM_REPO_ID"),
-    task=os.getenv("HF_LLM_TASK"),
-    max_new_tokens=int(os.getenv("HF_LLM_MAX_NEW_TOKENS", 2048))
+    repo_id="Qwen/Qwen2.5-7B-Instruct",
+    task="text-generation",
+    max_new_tokens=1000,
 )
+
 model = ChatHuggingFace(llm=llm)
 
 async def agent(message:str):
