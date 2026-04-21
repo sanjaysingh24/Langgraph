8d216eb6c59bf0772661e9d6a87fa6a541cb0c09
sanjaysingh24 <sanjubora84@gmail.com>
2026-04-21 21:01:24 +0530

    remove doker file

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from src.routes.llmroute import llmrouter
 app = FastAPI()
-
+#this is the tutorial for langgraph sequintial workflow
 app.include_router(llmrouter)
 @app.get("/")
 def read_root():
