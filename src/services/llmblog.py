42ae58e582108044d3cca0ce2b98016ccce0ae9e
sanjaysingh24 <sanjubora84@gmail.com>
2026-04-23 21:34:33 +0530

    update

--- src/services/llmblog.py
+++ src/services/llmblog.py
@@ -19,7 +19,26 @@ def create_outline(state:BlogState)->BlogState:
     outline =  model.invoke(prompt).content
     state['outline'] = outline
     return state
-  
+def create_blog(state:BlogState)->BlogState:
+    outline = state['outline']
+    #form a prompt
+    prompt = f"Write a blog post based on the following outline:{outline}"
+    blog = model.invoke(prompt).content
+    state['content'] = blog
+    return state
 def generate_blog(message:str):
     graph.add_node("create_outline",create_outline)
     graph.add_node("create_blog",create_blog)
+
+    #add edges
+    graph.add_edge(START,"create_outline")
+    graph.add_edge("create_outline","create_blog")
+    graph.add_edge("create_blog",END)
+
+    #compile the graph
+    workflow = graph.compile()
+
+    #execute the graph
+    initial_state ={'title':message}
+    final_state = workflow.invoke(initial_state)
+    print(final_state)
