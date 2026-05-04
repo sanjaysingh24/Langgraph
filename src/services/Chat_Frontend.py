388ed22e39128fd22648637646d944897947823c
sanjaysingh24 <sanjubora84@gmail.com>
2026-05-04 20:23:50 +0530

    update

--- src/services/Chat_Frontend.py
+++ src/services/Chat_Frontend.py
@@ -1,17 +1,15 @@
 import streamlit as st
-
-with st.chat_message('user'):
-    st.text('Hii bro')
-
-with st.chat_message('assistant'):
-    st.text("How can i help You")
-
-with st.chat_message('user'):
-    st.text("My name is Sanjay")
-
-
+#session state->dict -> not erase or reset and only reset the things when we erase this one
+message_history=[]
+for message in message_history:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
 user_input =st.chat_input("Type Here....")
 
 if user_input:
+    message_history.append({'role':"user",'content':user_input})
     with st.chat_message('user'):
+        st.text(user_input)
+    message_history.append({'role':"user",'content':user_input})
+    with st.chat_message("assistant"):
         st.text(user_input)
