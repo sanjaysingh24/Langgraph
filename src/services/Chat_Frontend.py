933266f1d1478f521108d463ee9f0607c63b93a9
sanjaysingh24 <sanjubora84@gmail.com>
2026-05-07 21:43:07 +0530

    update

--- src/services/Chat_Frontend.py
+++ src/services/Chat_Frontend.py
@@ -1,5 +1,9 @@
 import streamlit as st
+from Chat_backend import chatbot
+from langchain_core.messages import HumanMessage
 #session state->dict -> not erase or reset and only reset the things when we erase this one
+thread_id =1
+config = {'configurable':{'thread_id':thread_id}}
 if 'message_history' not in st.session_state:
     st.session_state['message_history'] =[]
 for message in st.session_state['message_history']:
@@ -11,6 +15,8 @@ if user_input:
     st.session_state['message_history'].append({'role':"user",'content':user_input})
     with st.chat_message('user'):
         st.text(user_input)
-    st.session_state["message_history"].append({'role':"assistant",'content':user_input})
+    response = chatbot.invoke({"messages":[HumanMessage(content=user_input)]},config=config)
+    ai_message = response['messages'][-1].content
+    st.session_state["message_history"].append({'role':"assistant",'content':ai_message})
     with st.chat_message("assistant"):
-        st.text(user_input)
+        st.text(ai_message)
--- src/services/Chat_backend.py
+++ src/services/Chat_backend.py
@@ -3,16 +3,19 @@ from dotenv import load_dotenv
 from langgraph.graph import StateGraph,START,END
 from typing import TypedDict,Annotated
 from langchain_core.messages import BaseMessage
-from langchain_huggingface import ChatHuggingFace
-from src.config.llm import AgentConfig
+from langchain_huggingface import ChatHuggingFace,HuggingFaceEndpoint
+
 from langgraph.graph.message import add_messages
-from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.checkpoint.memory import MemorySaver
 load_dotenv()
 
 #initalize the chat model one
-Textllm = AgentConfig()
-llm_config = Textllm.llm
-model = ChatHuggingFace(llm=llm_config)
+llm = HuggingFaceEndpoint( 
+    repo_id="Qwen/Qwen2.5-7B-Instruct",
+            task="text-generation",
+            max_new_tokens=2048)
+
+model = ChatHuggingFace(llm=llm)
 
 #define the state then 
 class ChatState(TypedDict):
@@ -21,7 +24,19 @@ class ChatState(TypedDict):
 #initalize the graph
 #initalize the checkpointer as well
 
-checkpointer = InMemorySaver()
+checkpointer = MemorySaver()
 graph = StateGraph(ChatState)
 
 
+#add nodes 
+def chat_node(state:ChatState):
+    message = state['messages']
+    response = model.invoke(message)
+    return{'messages':[response]}
+
+graph.add_node("chat_node",chat_node)
+
+graph.add_edge(START,"chat_node")
+graph.add_edge("chat_node",END)
+
+chatbot = graph.compile(checkpointer=checkpointer)
