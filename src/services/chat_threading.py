ca2992f97e0f9975dd9775b10c12a0d0cbaa166f
sanjaysingh24 <sanjubora84@gmail.com>
2026-05-11 12:28:02 +0530

    update

---
+++ src/services/chat_threading.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from Chat_backend import chatbot
+from langchain_core.messages import HumanMessage
+#session state->dict -> not erase or reset and only reset the things when we erase this one
+thread_id =1
+config = {'configurable':{'thread_id':thread_id}}
+if 'message_history' not in st.session_state:
+    st.session_state['message_history'] =[]
+for message in st.session_state['message_history']:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
+user_input =st.chat_input("Type Here....")
+
+if user_input:
+    st.session_state['message_history'].append({'role':"user",'content':user_input})
+    with st.chat_message('user'):
+        st.text(user_input)
+
+
+    with st.chat_message("assistant"):
+       ai_message =  st.write_stream(
+            message_chunk.content for message_chunk, metadata in chatbot.stream({"messages":[HumanMessage(content=user_input)]},config=config,
+                           stream_mode="messages")
+        )
+    st.session_state["message_history"].append({'role':"assistant",'content':ai_message})
